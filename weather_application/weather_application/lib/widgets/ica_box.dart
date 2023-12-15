dddb0a1403efd9d231ceb571c4f8ebd88b9d6098
Joel Romero <joelromero.teke@gmail.com>
2023-12-15 18:31:39 +0100

    Solved bugs on ica box widget

--- weather_application/weather_application/lib/widgets/ica_box.dart
+++ weather_application/weather_application/lib/widgets/ica_box.dart
@@ -6,11 +6,34 @@ class IcaWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      width: 40,
-      height: 6,
+      width: 110,
+      height: 40,
       decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 139, 139, 139),
-        borderRadius: BorderRadius.circular(4),
+        color: const Color.fromARGB(255, 177, 176, 176),
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 10.0),
+            child: Icon(
+              Icons.eco,
+              color: Colors.green,
+            ),
+          ),
+          Center(
+            child: Text(
+              'ICA 42',
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          SizedBox(width: 10),
+        ],
       ),
     );
   }
