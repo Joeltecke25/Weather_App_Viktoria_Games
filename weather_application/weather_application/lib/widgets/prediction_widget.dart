396a1f39f917a16a8adbe64c6397960ab98791af
Joel Romero <joelromero.teke@gmail.com>
2023-12-17 01:13:25 +0100

    Implementation Info for Prediction Widget

--- weather_application/weather_application/lib/widgets/prediction_widget.dart
+++ weather_application/weather_application/lib/widgets/prediction_widget.dart
@@ -16,6 +16,60 @@ class PredictionBox extends StatelessWidget {
           color: const Color.fromARGB(100, 207, 207, 207),
           borderRadius: BorderRadius.circular(30),
         ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(height: 16),
+            buildTextWithSeparator('7-DAY FORECAST'),
+            buildTextWithSeparator('Today'),
+            buildTextWithSeparator('Tue'),
+            buildTextWithSeparator('Wed'),
+            buildTextWithSeparator('Thu'),
+            buildTextWithSeparator('Fri'),
+            buildTextWithSeparator('Sat'),
+            buildTextWithoutSeparator('Sun'),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildTextWithSeparator(String text) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 20, top: 14, bottom: 14),
+          child: Text(
+            text,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+        ),
+        if (text != 'Sun')
+          Container(
+            margin: const EdgeInsets.symmetric(horizontal: 20),
+            height: 1,
+            width: double.infinity,
+            color: Colors.white,
+          ),
+      ],
+    );
+  }
+
+  Widget buildTextWithoutSeparator(String text) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 20, top: 14, bottom: 14),
+      child: Text(
+        text,
+        style: const TextStyle(
+          color: Colors.white,
+          fontSize: 16,
+          fontWeight: FontWeight.bold,
+        ),
       ),
     );
   }
