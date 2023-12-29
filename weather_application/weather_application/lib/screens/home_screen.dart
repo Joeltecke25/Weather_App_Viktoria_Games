b9a0143bcdabf3940684db7c28482e2cfaa4bf88
Joel Romero <joelromero.teke@gmail.com>
2023-12-29 22:51:05 +0100

    Info and Settings Icon (Home Screen)

--- weather_application/weather_application/lib/screens/home_screen.dart
+++ weather_application/weather_application/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:weather_application/widgets/home_widgets/home_location_info.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -9,13 +10,32 @@ class HomeScreen extends StatelessWidget {
       body: Container(
         decoration: const BoxDecoration(
           gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 0, 153, 255),
-              Color.fromARGB(255, 255, 135, 37),
-            ],
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-          ),
+                colors: [
+                  Color.fromARGB(255, 0, 153, 255),
+                  Color.fromARGB(255, 255, 135, 37),
+                ],
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+              ),
+        ),
+        child: Stack(
+          children: [
+            Positioned(
+              top: 20.0,
+              right: 20.0,
+              child: IconButton(
+                icon: const Icon(Icons.settings, color: Colors.white),
+                onPressed: () {
+                },
+              ),
+            ),
+            Positioned(
+              top: 40.0,
+              width: MediaQuery.of(context).size.width,
+              height: 150,
+              child: const LocationInfoHomeWidget(),
+            ),
+          ],
         ),
       ),
     );
