88dc15b238e4d9a9fb3c7dd9f4f3c9dc67019456
DonnoNonno <paugarrigab@gmail.com>
2023-12-30 19:59:28 +0100

    Sunset box receives info from api

--- weather_application/weather_application/lib/widgets/home_widgets/sunset_box.dart
+++ weather_application/weather_application/lib/widgets/home_widgets/sunset_box.dart
@@ -2,10 +2,18 @@ import 'package:flutter/material.dart';
 
 class SunsetBox extends StatelessWidget {
   final Map<String, dynamic>? futureweatherData;
-  const SunsetBox({super.key, required this.futureweatherData});
+
+  const SunsetBox({Key? key, required this.futureweatherData})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    // Accessing sunset and sunrise times from the API data
+    String? sunsetTime =
+        futureweatherData?['forecast']['forecastday'][0]['astro']['sunset'];
+    String? sunriseTime =
+        futureweatherData?['forecast']['forecastday'][0]['astro']['sunrise'];
+
     return Container(
       height: 180,
       width: 180,
@@ -14,10 +22,10 @@ class SunsetBox extends StatelessWidget {
         borderRadius: BorderRadius.circular(20),
       ),
       padding: const EdgeInsets.all(16),
-      child: const Column(
+      child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Row(
+          const Row(
             children: [
               Icon(
                 Icons.wb_twilight,
@@ -31,24 +39,26 @@ class SunsetBox extends StatelessWidget {
               ),
             ],
           ),
-          SizedBox(height: 8),
+          const SizedBox(height: 8),
           Text(
-            "18:03",
-            style: TextStyle(fontSize: 30, color: Colors.white),
+            "$sunsetTime", // Display sunset time
+            style: const TextStyle(fontSize: 30, color: Colors.white),
           ),
-          SizedBox(height: 50),
+          const SizedBox(height: 50),
           Row(
             mainAxisAlignment: MainAxisAlignment.start,
             children: [
-              Text(
+              const Text(
                 "Sunrise",
                 style: TextStyle(
-                    fontSize: 16, color: Color.fromARGB(255, 221, 221, 221)),
+                  fontSize: 16,
+                  color: Color.fromARGB(255, 221, 221, 221),
+                ),
               ),
-              SizedBox(width: 8),
+              const SizedBox(width: 8),
               Text(
-                "07:11",
-                style: TextStyle(fontSize: 16, color: Colors.white),
+                "$sunriseTime", // Display sunrise time
+                style: const TextStyle(fontSize: 16, color: Colors.white),
               ),
             ],
           ),
