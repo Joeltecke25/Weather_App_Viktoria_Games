4fc46a6d05cdc7b5181171e6828a8a9ab860dd83
Joel Romero <joelromero.teke@gmail.com>
2024-01-11 22:14:15 +0100

    Arrow introduced on Wind Direction

--- weather_application/weather_application/lib/widgets/wind_widgets/wind_direction.dart
+++ weather_application/weather_application/lib/widgets/wind_widgets/wind_direction.dart
@@ -4,63 +4,69 @@ class WindDirection extends StatelessWidget {
   const WindDirection({super.key});
 
   @override
-Widget build(BuildContext context) {
-  double screenWidth = MediaQuery.of(context).size.width;
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
 
-  return Padding(
-    padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-    child: Container(
-      width: screenWidth - 62,
-      height: 400,
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(100, 207, 207, 207),
-        borderRadius: BorderRadius.circular(30),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          const Padding(
-            padding: EdgeInsets.fromLTRB(16, 16, 0, 0),
-            child: Text(
-              'Wind direction',
-              style: TextStyle(
-                fontSize: 16,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+      child: Container(
+        width: screenWidth - 62,
+        height: 400,
+        decoration: BoxDecoration(
+          color: const Color.fromARGB(100, 207, 207, 207),
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.fromLTRB(16, 16, 0, 0),
+              child: Text(
+                'Wind direction',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
               ),
             ),
-          ),
-          Expanded(
-            child: Center(
-              child: Container(
-                width: 300,
-                height: 300,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  border: Border.all(
-                    color: const Color.fromARGB(255, 255, 255, 255),
-                    width: 10,
+            Expanded(
+              child: Center(
+                child: Container(
+                  width: 300,
+                  height: 300,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    border: Border.all(
+                      color: const Color.fromARGB(255, 255, 255, 255),
+                      width: 10,
+                    ),
                   ),
-                ),
-                child: Center(
-                  child: Container(
-                    width: 250,
-                    height: 250,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      border: Border.all(
-                        color: const Color.fromARGB(255, 190, 190, 190),
-                        width: 5,
-                      )
+                  child: Center(
+                    child: Container(
+                      width: 250,
+                      height: 250,
+                      decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: const Color.fromARGB(255, 190, 190, 190),
+                            width: 5,
+                          )),
+                      child: const Center(
+                        child: Icon(
+                          Icons.arrow_forward,
+                          size: 100,
+                          color: Colors.white,
+                        ),
+                      ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
-    ),
-  );
-}
+    );
+  }
 }
