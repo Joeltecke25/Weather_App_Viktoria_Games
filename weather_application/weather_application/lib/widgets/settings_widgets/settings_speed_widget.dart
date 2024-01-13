347041bde22adfbbdb4e96837f8eb848bd9f09e3
IvanBSupc <ivan.bermudez@estudiantat.upc.edu>
2024-01-13 18:09:22 +0100

    Settings Screen - Speed Widget

---
+++ weather_application/weather_application/lib/widgets/settings_widgets/settings_speed_widget.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class SettingsSpeed extends StatelessWidget {
+  const SettingsSpeed({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 70,
+      decoration: const BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 139, 139, 139),
+            offset: Offset(0, 0),
+            blurRadius: 4.0,
+          ),
+        ],
+      ),
+      child: const Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: EdgeInsets.only(left: 25),
+            child: Text(
+              'Unit',
+              style: TextStyle(
+                color: Colors.black,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(right: 25),
+            child: Text(
+              'km/h',
+              style: TextStyle(
+                color: Color.fromARGB(255, 180, 180, 180),
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
