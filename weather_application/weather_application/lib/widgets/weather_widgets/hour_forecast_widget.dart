a93b6839eec91d5bed159b96c93946f013497b69
Joel Romero <joelromero.teke@gmail.com>
2024-01-14 23:11:09 +0100

    Update hour_forecast_widget.dart

--- weather_application/weather_application/lib/widgets/weather_widgets/hour_forecast_widget.dart
+++ weather_application/weather_application/lib/widgets/weather_widgets/hour_forecast_widget.dart
@@ -72,17 +72,45 @@ class HourForecastWidget extends StatelessWidget {
   }
 
   Icon getWeatherIcon(String condition) {
-    switch (condition) {
-      case 'Sunny':
+    switch (condition.toLowerCase()) {
+      case 'sunny':
         return const Icon(Icons.wb_sunny_outlined,
             size: 40, color: Colors.white);
-      case 'Cloudy':
+      case 'clear':
+        return const Icon(Icons.wb_sunny_outlined,
+            size: 40, color: Colors.white);
+      case 'partly cloudy':
+        return const Icon(Icons.wb_cloudy_outlined,
+            size: 40, color: Colors.white);
+      case 'cloudy':
         return const Icon(Icons.cloud_outlined, size: 40, color: Colors.white);
-      case 'Rainy':
+      case 'rainy':
         return const Icon(Icons.grain_outlined, size: 40, color: Colors.white);
-      case 'Snowy':
+      case 'snowy':
         return const Icon(Icons.ac_unit_outlined,
             size: 40, color: Colors.white);
+      case 'thunderstorm':
+        return const Icon(Icons.flash_on_outlined,
+            size: 40, color: Colors.white);
+      case 'foggy':
+        return const Icon(Icons.filter_drama_outlined,
+            size: 40, color: Colors.white);
+      case 'windy':
+        return const Icon(Icons.waves_outlined, size: 40, color: Colors.white);
+      case 'hazy':
+        return const Icon(Icons.blur_on_outlined,
+            size: 40, color: Colors.white);
+      case 'misty':
+        return const Icon(Icons.cloud_outlined, size: 40, color: Colors.white);
+      case 'smoky':
+        return const Icon(Icons.smoke_free_outlined,
+            size: 40, color: Colors.white);
+      case 'dust':
+        return const Icon(Icons.cloud_outlined, size: 40, color: Colors.white);
+      case 'sand':
+        return const Icon(Icons.beach_access_outlined,
+            size: 40, color: Colors.white);
+      // Add more cases as needed
       default:
         return const Icon(Icons.help, size: 40, color: Colors.black);
     }
