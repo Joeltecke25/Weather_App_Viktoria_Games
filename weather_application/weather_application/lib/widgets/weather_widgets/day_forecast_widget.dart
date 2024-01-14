04782bb391e0c9fd7dbc7d85f57df248dcb22983
DonnoNonno <paugarrigab@gmail.com>
2024-01-14 09:41:07 +0100

    day forecast api and week choose function

--- weather_application/weather_application/lib/widgets/weather_widgets/day_forecast_widget.dart
+++ weather_application/weather_application/lib/widgets/weather_widgets/day_forecast_widget.dart
@@ -9,6 +9,34 @@ class DayForecast extends StatelessWidget {
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
 
+    List<Map<String, dynamic>>? forecastDays =
+        futureweatherData?['forecast']['forecastday'];
+
+    String? tomorrowDay = forecastDays?[1]['date_epoch'];
+    String? afterTomorrowDay = forecastDays?[2]['date_epoch'];
+
+    String getDayOfWeek(String? epochTime) {
+      if (epochTime != null) {
+        DateTime date =
+            DateTime.fromMillisecondsSinceEpoch(int.parse(epochTime) * 1000);
+        List<String> daysOfWeek = [
+          'Monday',
+          'Tuesday',
+          'Wednesday',
+          'Thursday',
+          'Friday',
+          'Saturday',
+          'Sunday'
+        ];
+        return daysOfWeek[date.weekday - 1];
+      }
+      return '';
+    }
+
+    String getTemperature(String? temperature) {
+      return temperature ?? '';
+    }
+
     return Padding(
       padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
       child: Container(
@@ -25,7 +53,9 @@ class DayForecast extends StatelessWidget {
               children: [
                 const SizedBox(height: 16),
                 buildRowWithIconAndText(
-                    Icons.calendar_today_rounded, '3-DAY FORECAST'),
+                  Icons.calendar_today_rounded,
+                  '3-DAY FORECAST',
+                ),
                 const SizedBox(height: 16),
               ],
             ),
@@ -34,13 +64,26 @@ class DayForecast extends StatelessWidget {
               right: 80,
               child: Row(
                 children: [
-                  buildTextWithSeparatorRight('Today', Icons.wb_sunny, '10º'),
+                  buildTextWithSeparatorRight(
+                    'Today',
+                    Icons.wb_sunny,
+                    getTemperature(
+                        forecastDays?[0]['day']['maxtemp_c']?.toString()),
+                  ),
                   const SizedBox(width: 20),
                   buildTextWithSeparatorRight(
-                      'Tuesday', Icons.wb_cloudy, '12º'),
+                    getDayOfWeek(tomorrowDay),
+                    Icons.wb_cloudy,
+                    getTemperature(
+                        forecastDays?[1]['day']['maxtemp_c']?.toString()),
+                  ),
                   const SizedBox(width: 20),
                   buildTextWithoutSeparatorRight(
-                      'Wednesday', Icons.wb_sunny, '15º'),
+                    getDayOfWeek(afterTomorrowDay),
+                    Icons.wb_sunny,
+                    getTemperature(
+                        forecastDays?[2]['day']['maxtemp_c']?.toString()),
+                  ),
                 ],
               ),
             ),
